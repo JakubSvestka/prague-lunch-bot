13033b1705844cbb6454de801edd8c97ba2b6caf
Jakub Švestka <svestka1999@gmail.com>
2025-07-28 20:04:18 +0200

    Remove a star from name

--- src/scrappers/pivokarlin.ts
+++ src/scrappers/pivokarlin.ts
@@ -23,10 +23,10 @@ export async function fetchPivokarlin(scrapper: Scrapper): Promise<Menu> {
             .each((index, itemEl) => {
                 const fullName = $(itemEl).find(".item_name h4").text().trim()
                 const name = fullName
+                    .replace("*", "")
                     .replace(/\s[0-9]{1,2}[a-z]?([,][0-9]{1,2}[a-z]?)*$/, "")
                     .replace(/\d+(g|(ks))/, "")
                     .replace(/VEG\s+/, "")
-                    .replace("*", "")
                     .trim()
                 const priceText = $(itemEl).find(".item_price h4").text().trim().replace(",-", "").trim()
                 const price = parseInt(priceText, 10)
