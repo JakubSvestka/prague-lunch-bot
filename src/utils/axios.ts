4da029b6e360414b221ac9621aef76e058f14ff5
Jakub Švestka <svestka1999@gmail.com>
2026-04-20 10:23:08 +0200

    Disable `rejectUnauthorized`

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
+import https from "https";
 
 
 const http = axios.create({
     timeout: 5000, // 5 seconds timeout per request
+    httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+    }),
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
